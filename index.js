6eac412f80a4d6154c92b38e63c00d266a48f14b
Animechill30 <82389963+AmerUkmata@users.noreply.github.com>
2022-06-19 14:56:36 +0200

    a

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+    return a + b
+  }
+
+  function isEven(a) {
+    // write your code here
+    if (a % 2 == 0) return true;
+    else return false;
+  }
+  
+  function greet(name) {
+    // write your code here
+    return `Hello there ${name}`
+  }
+  
+  function isAnAdult(age) {
+    // write your code here
+    if (age >= 18) return true
+    else return false 
+  }
+  
+  function yearsToAdulthood(age) {
+   let yearsLeft = 18 - age;
+   if(yearsLeft <= 0 ){
+    return `You are already an adult, fam`
+   }
+   else {
+   if(yearsLeft === 1)
+   return `${yearsLeft} year`
+   else return `${yearsLeft} years`
+  }
+  }
+  
+  function admit(person) {
+    if (isAnAdult(person.age)) console.log(`Welcome ${person.name}`) 
+    else console.log(`Come back in ${yearsToAdulthood(person.age)} when you are old enough!`)
+  }
+  let amer = {name: "Amer", age: 18};
+  admit(amer);
